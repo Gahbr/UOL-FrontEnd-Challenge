37f35a560a754ae95b68432a9a8106a3d31478b6
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2023-01-21 13:08:49 -0300

    fix: lista em grid

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -35,34 +35,32 @@ export default function UserList() {
                 <div className='text-xl text-slate-700 mt-7 mb-3 flex'>Listagem de Usuários</div>
                 <div className='text-lg text-gray-500 mb-14 flex'> Escolha um cliente para visualizar os detalhes</div>
             </div>
-            <div className='flex flex-col justify-center align-middle pr-5'>
+            <div className='flex flex-col justify-center align-middle pr-6'>
                 <Button.Main className='w-28 h-9 text-base' name='Novo Cliente' onClick={()=> handleRoute("/signup")}/>
                
             </div>
         </div>
         {user.map((item, index)=>(
-          <div className='border flex justify-between pl-8 pr-9 pt-5 pb-5 mb-7'>
-            <div className='flex-row '>
+          <div className="grid grid-cols-4 gap-80 border  pl-8 pr-9 pt-5 pb-5 mb-7 whitespace-nowrap" key={index}>
+           <div className='flex-col '>
               <div className='text-lg  text-gray-500'><b>{item.name}</b></div>
               <div className='text-lg  text-gray-500'>{item.email}</div>
             </div>
-
-            <div className='flex-row'>
-              <div className='text-lg  text-gray-500'><b>{item.cpf}</b></div>
-              <div className='text-lg  text-gray-500'>{item.phone}</div>
+            <div className=' inline-flex flex-col w-32'>
+              <div className='text-lg  text-gray-500 flex-col'><b>{item.cpf}</b></div>
+              <div className='text-lg  text-gray-500 flex-col'>{item.phone}</div>
             </div>
-
-          {item.status == 'ativo'&&
+            {item.status == 'ativo'&&
             <div className='flex-col self-center'>
-              <span className='h-3 w-3 mr-1 rounded-full bg-green-500 inline-block'></span>
-              <span className='text-lg  text-gray-500'>{item.status}</span>
+              <span className='h-3 w-3 mr-1 rounded-full bg-green-500 inline-block '></span>
+              <span className='text-lg  text-gray-500'>Ativo</span>
             </div>
           }
 
           {item.status == 'inativo' &&
           <div className='flex-col self-center'>
             <span className='h-3 w-3 mr-1 rounded-full bg-red-500 inline-block'></span>
-            <span className='text-lg  text-gray-500'>{item.status}</span>
+            <span className='text-lg  text-gray-500'>Inativo</span>
           </div>
           }
 
@@ -83,11 +81,11 @@ export default function UserList() {
             <div className=' flex flex-row self-center'>
               <Button.Alternative name='Editar' onClick={()=> alert("yamete")}/>
             </div>
-        </div> 
+        </div>
         ))}
         
         
-       <div> Exibindo {user.length} clientes</div>
+       <div className='text-base  text-gray-500'> Exibindo {user.length} clientes</div>
     </div>
   )
 }
